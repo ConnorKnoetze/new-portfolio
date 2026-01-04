9ac3c20620fa1f0cd03dbbe66abc800dd5c046eb
connorknoetze <connorknoetze@gmail.com>
2026-01-04 22:11:01 +1300

    feat(wallpaper/projects) added to wallpaper panel and created projects panel

--- src/components/WallpaperPanel/WallpaperPanel.js
+++ src/components/WallpaperPanel/WallpaperPanel.js
@@ -2,30 +2,36 @@ import WindowControls from "../WindowControls/WindowControls";
 import "@/styles/WallpaperPanel/WallpaperPanel.css";
 
 const wallpaperFiles = [
-    "20250827224255_1.jpg",
-    "2f88vbbi9kue195.png",
-    "788rircudz0c1.png",
-    "9213o4hips3d1.jpg",
-    "9inv0o6ek0qd1.png",
-    "ai8l9hbf6tkd1.png",
-    "DnDGo1.png",
-    "kjm887zhqjie1.png",
-    "van-Gogh-Almond-Blossom-1890.jpg",
-    "yo33qdyyfozc1.jpeg",
-    "z7t5x3p9olud1.png"
+    ["Screenshot 2026-01-04 154925.png", "2f88vbbi9kue195.png"],
+    ["Screenshot 2026-01-04 154936.png", "9inv0o6ek0qd1.png"],
+    ["Screenshot 2026-01-04 154959.png", "788rircudz0c1.png"],
+    ["Screenshot 2026-01-04 155015.png", "9213o4hips3d1.jpg"],
+    ["Screenshot 2026-01-04 155025.png", "20250827224255_1.jpg"],
+    ["Screenshot 2026-01-04 155041.png", "ai8l9hbf6tkd1.png"],
+    ["Screenshot 2026-01-04 155051.png", "DnDGo1.png"],
+    ["Screenshot 2026-01-04 155104.png", "kjm887zhqjie1.png"],
+    ["Screenshot 2026-01-04 155118.png", "van-Gogh-Almond-Blossom-1890.jpg"],
+    ["Screenshot 2026-01-04 155131.png", "yo33qdyyfozc1.jpeg"],
+    ["Screenshot 2026-01-04 155142.png", "z7t5x3p9olud1.png"]
 ];
 
 export default function WallpaperPanel(){
+
+    function setAsWallpaper(wallpaperFileName) {
+        const backgroundUrl = `url('/images/wallpapers/${wallpaperFileName}')`;
+        document.documentElement.style.setProperty('--background', backgroundUrl);
+    }
+
     return (
-        <div className="wallpaper-panel-content">
+        <div className="wallpaper-panel-content" onClick={(e) => e.stopPropagation()}>
             <div className="wallpaper-panel-header">
-                <WindowControls/>
+                <WindowControls panelName="wallpaper"/>
             </div>
             <div className="wallpaper-panel-body">
                 <div className="wallpaper-panel-grid">
-                    {wallpaperFiles.map((file) => (
-                        <div key={file} className="wallpaper-item">
-                            <img src={`/images/wallpapers/${file}`} alt={`Wallpaper ${file}`} className="wallpaper-image" loading="lazy" decoding="async"/>
+                    {wallpaperFiles.map(([thumbnail, wallpaper]) => (
+                        <div key={wallpaper} className="wallpaper-item" onClick={() => setAsWallpaper(wallpaper)}>
+                            <img src={`/images/wallpaper_thumbnails/${thumbnail}`} alt={`Wallpaper ${wallpaper}`} className="wallpaper-image" loading="lazy" decoding="async"/>
                             <p>Set As Wallpaper</p>
                         </div>
                     ))}
